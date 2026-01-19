57926a9b7148d09ac19eca9e4b7315b973f5b603
Seb_Leib <sebastien.leib1@gmail.com>
2026-01-19 16:36:39 +0800

    Add greeting

--- src/main/java/Marvin.java
+++ src/main/java/Marvin.java
@@ -1,10 +1,25 @@
 public class Marvin {
+    public static void greet() {
+        String logo = """
+                 __  __
+                |  \\/  |                 (_)
+                | \\  / | __ _ _ ____   ____ _ __
+                | |\\/| |/ _` | `__\\ \\ / /| | `_ \\
+                | |  | | (_| | |   \\ V / | | | | |\s
+                |_|  |_|\\__,_|_|    \\_/  |_|_| |_|\s
+                """;
+
+        System.out.println("Hello. I am Marvin.\n"
+                + logo
+                + "\nI have a brain the size of a planet,\n"
+                + "and here I am, greeting users.\n"
+                + "It’s all terribly depressing.\n"
+                + "\nAnyways, what can I do for you?\n"
+                + "____________________________________________________________"
+        );
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
