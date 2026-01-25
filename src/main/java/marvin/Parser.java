d99e62def1b0d7d581a0da89daa5d17af70bae0f
Seb_Leib <sebastien.leib1@gmail.com>
2026-01-25 19:55:20 +0800

    Add utility function for parsing commands

--- src/main/java/marvin/Parser.java
+++ src/main/java/marvin/Parser.java
@@ -1,5 +1,10 @@
 package marvin;
 
+import marvin.command.*;
+import marvin.task.Deadline;
+import marvin.task.Event;
+import marvin.task.Todo;
+
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -8,9 +13,81 @@ import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAccessor;
 
 /**
- * marvin.Parser class with static utility functions to parse strings into various types.
+ * Parser class with static utility functions to parse strings into various types.
  */
 public class Parser {
+    public static Command parseCommand(String commandString) throws MarvinException {
+        String[] command = commandString.trim().split("\\s+", 2);
+
+        CommandType commandWord = CommandType.from(command[0]);
+        String args = command.length > 1 ? command[1] : ""; // only handles one argument
+
+        String desc;
+
+        switch (commandWord) {
+            case EXIT:
+                return new ExitCommand();
+
+            case LIST:
+                return new ListCommand();
+
+            case MARK:
+                return new MarkCommand(Integer.parseInt(args));
+
+            case UNMARK:
+                return new UnmarkCommand(Integer.parseInt(args));
+
+            case DELETE:
+                return new DeleteCommand(Integer.parseInt(args));
+
+            case TODO:
+                if (args.isBlank()) {
+                    throw new MarvinException("A todo without a description is rather pointless.");
+                }
+
+                return new AddTaskCommand(new Todo(args));
+
+            case DEADLINE:
+                String[] split = args.split("/by", 2);
+                if (split.length < 2) {
+                    throw new MarvinException("Deadlines tend to require a deadline. Try using /by.");
+                }
+
+                desc = split[0].trim();
+
+                String by = split[1].trim();
+                if (desc.isEmpty() || by.isEmpty()) {
+                    throw new MarvinException("A deadline for nothing in particular is deeply confusing.");
+                }
+
+                return new AddTaskCommand(new Deadline(desc, by));
+
+            case EVENT:
+                String[] fromSplit = args.split("/from", 2);
+                if (fromSplit.length < 2) {
+                    throw new MarvinException("An event should probably start at some point. Try /from.");
+                }
+
+                desc = fromSplit[0].trim();
+
+                String[] toSplit = fromSplit[1].split("/to", 2);
+                if (toSplit.length < 2) {
+                    throw new MarvinException("Events usually end. Try specifying /to.");
+                }
+
+                String from = toSplit[0].trim();
+                String to = toSplit[1].trim();
+                if (desc.isEmpty() || from.isEmpty() || to.isEmpty()) {
+                    throw new MarvinException("An event with missing details is... incomplete.");
+                }
+
+                return new AddTaskCommand(new Event(desc, from, to));
+
+            default:
+                throw new MarvinException("I don’t know what you want me to do.");
+        }
+    }
+
     public static LocalDate parseDate(String token) {
         if (token == null) {
             throw new IllegalArgumentException("token must not be null");
